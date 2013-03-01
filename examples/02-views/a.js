5e61846a42755db907cfaf1598445fd54408ae46
Dimitris Baltas <dbaltas@travelplanet24.com>
2013-03-01 14:02:37 +0200

    render collection with partial views

--- examples/02-views/a.js
+++ examples/02-views/a.js
@@ -20,17 +20,47 @@ var search_view = new SearchView({ el: $("#search_container") });
   //- See more at: http://backbonetutorials.com/what-is-a-view/#sthash.7IW3AJSO.dpuf
 
 CustomerView = Backbone.View.extend({ 
-  template : _.template( $("#customer_template").html()),
+  template : _.template( $(".customer_template").html()),
   initialize: function(){ 
-    this.render(); 
   }, 
-  render: function() { // Compile the template using underscore 
-  // Load the compiled HTML into the Backbone "el" 
-  console.log(this.model.toJSON());
+  render: function() {
     this.$el.html( this.template(this.model.toJSON()) ); 
+    return this;
   } 
 }); 
 
 var John = customers.first();
 var john_view = new CustomerView({ el: $("#john_container"), model : John }); 
+john_view.render();
 
+
+CustomerCollectionView = Backbone.View.extend({
+  initialize : function(){
+    var that = this;
+    console.log(this.el);
+    this._customerViews = [];
+    this.collection.each(function(customer){
+      that._customerViews.push(
+        new CustomerView({
+          model : customer,
+          tagName : 'li'
+        })
+      );
+    });
+  }, 
+  render : function() {
+    var that = this;
+    $(this.el).empty();
+    _(this._customerViews).each(function(cv){
+      console.log(cv.render());
+      $(that.$el).append(cv.render().el)
+    });
+  }
+});
+
+var customerCollectionView = new CustomerCollectionView({
+  collection : customers,
+  el : $('ul.customers')[0]
+});
+
+customerCollectionView.render();
